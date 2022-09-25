f913a1e6359b9994da56d7c1036034d1fa59db78
slowlydownwardz <107696381+slowlydownwardz@users.noreply.github.com>
2022-09-25 21:48:59 +0100

    Update landing.js

--- landing.js
+++ landing.js
@@ -79,8 +79,7 @@ const loc = document.querySelector('#location');
 const tempC = document.querySelector('.c');
 const tempF = document.querySelector('.f');
 const desc = document.querySelector('.desc');
-const sunriseDOM = document.querySelector('.sunrise');
-const sunsetDOM = document.querySelector('.sunset');
+
 
 window.addEventListener('load', () => {
   let long;
@@ -102,14 +101,9 @@ window.addEventListener('load', () => {
           const { temp } = data.main;
           const place = data.name;
           const { description, icon } = data.weather[0];
-          const { sunrise, sunset } = data.sys;
 
           const iconUrl = `http://openweathermap.org/img/wn/${icon}@2x.png`;
           const fahrenheit = (temp * 9) / 5 + 32;
-
-          // Converting Epoch(Unix) time to GMT
-          const sunriseGMT = new Date(sunrise * 1000);
-          const sunsetGMT = new Date(sunset * 1000);
 
           // Interacting with DOM to show data
           iconImg.src = iconUrl;
@@ -117,8 +111,7 @@ window.addEventListener('load', () => {
           desc.textContent = `${description}`;
           tempC.textContent = `${temp.toFixed(2)} °C`;
           tempF.textContent = `${fahrenheit.toFixed(2)} °F`;
-          sunriseDOM.textContent = `${sunriseGMT.toLocaleDateString()}, ${sunriseGMT.toLocaleTimeString()}`;
-          sunsetDOM.textContent = `${sunsetGMT.toLocaleDateString()}, ${sunsetGMT.toLocaleTimeString()}`;
+
         });
     });
   }
